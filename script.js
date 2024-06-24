598e78ef4c44ce7dccb9996da64e4a74076bd447
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-24 13:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// let input=document.querySelectorAll("input");
+// alert("hi"+input[0].value);
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+
+alert(age.value,name.value); 
