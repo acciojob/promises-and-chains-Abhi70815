750b387d245a71884445374226555c0a668cd3b0
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-24 18:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let form = document.getElementById("myForm");
-form.addEventListener('submit', () => {
-    let age = document.getElementById("age").value;
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    let age = parseInt(document.getElementById("age").value);
     let name = document.getElementById("name").value;
-    age = parseInt(age);
 
-    let myPromise = new Promise((resolve, reject)=> {
+    let myPromise = new Promise((resolve, reject) => {
         if (age > 18) {
-			setTimeout(()=>{
-
-            resolve(`Welcome, ${name}. You can vote.`);
-			},4000);
+            setTimeout(() => {
+                resolve(`Welcome, ${name}. You can vote.`);
+            }, 4000);
         } else {
             reject(`Oh sorry, ${name}. You aren't old enough.`);
         }
     });
 
-//     myPromise.then(
-//         function (value) {
+    myPromise.then(
+        function (value) {
 			
 				
-//             alert(value);
+            alert(value);
 				
 			
-//         },
-//         function (error) {
-//             alert(error);
-//         }
-//     );
+        },
+        function (error) {
+            alert(error);
+        }
+    );
 
-// });
+});
