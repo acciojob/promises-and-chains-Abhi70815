5aa7e77dd3106fdeed75a96b4c628f1d500ede35
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-08 15:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function prom() {
-    let form = document.getElementById("myForm");
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevent form submission
+// function prom() {
+//     let form = document.getElementById("myForm");
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault(); // Prevent form submission
 
-        let age = parseInt(document.getElementById("age").value);
-        let name = document.getElementById("name").value;
+//         let age = parseInt(document.getElementById("age").value);
+//         let name = document.getElementById("name").value;
 		
 
-        return new Promise((resolve, reject) => {
-            if (age > 18) {
-                setTimeout(() => {
-                    alert(`Welcome, ${name}. You can vote.`);
-                    resolve();
-                }, 4000);
-            } else {
-                alert(`Oh sorry ${name}. You aren't old enough.`);
-                reject();
-            }
-        });
-    });
-}
+//         return new Promise((resolve, reject) => {
+//             if (age > 18) {
+//                 setTimeout(() => {
+//                     alert(`Welcome, ${name}. You can vote.`);
+//                     resolve();
+//                 }, 4000);
+//             } else {
+//                 alert(`Oh sorry ${name}. You aren't old enough.`);
+//                 reject();
+//             }
+//         });
+//     });
+// }
 
-async function pro() {
-    await prom();
-}
-pro();
+// async function pro() {
+//     await prom();
+// }
+// pro();
